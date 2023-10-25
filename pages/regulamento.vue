e18e8ca65b4f48f35dc040238ba4aeab18c546ee
Carlos Henrique Gomes <carlosgomes.2000@alunos.utfpr.edu.br>
2023-10-24 21:42:40 -0300

    Implementação da tela de regulamentação

--- pages/regulamento.vue
+++ pages/regulamento.vue
@@ -1,5 +1,39 @@
 <template>
-  <div>Regulamentação</div>
+  <div
+    class="flex flex-col max-[320px]:p-10 p-12 lg:p-20 text-center max-[320px]:space-y-4 space-y-6 md:space-y-8 lg:space-y-8"
+  >
+    <h1
+      class="text-[#004c25] text-2xl md:text-4xl lg:text-5xl font-bold max-[320px]:text-xl"
+    >
+      Regulamentação
+    </h1>
+    <p
+      class="md:pl-16 md:pr-16 lg:pl-40 lg:pr-40 text-md md:text-2xl lg:text-3xl max-[320px]:text-xs"
+    >
+      A validade da promoção é indeterminada, ficando a critério da Franet
+      Telecom a sua continuidade. <br />
+      A promoção é cumulativa: o Cliente indicador ganha 50% de desconto em uma
+      mensalidade a cada nova indicação e contratação da Franet Telecom, por
+      assinante indicado. <br />
+      O desconto é concedido ao Cliente Indicador após assinatura de contrato de
+      fidelidade pelo Assinante Indicado. O valor do desconto adquirido pode
+      quitar débitos com a mesma proporção de 50%, sobre o valor da mensalidade
+      devida. No caso de indicações duplicadas por clientes diferentes, será
+      considerada apenas a primeira, de acordo com a data de preenchimento do
+      formulário, realizando, para tanto, análises de data e horário. A análise
+      das informações terá como base a data e horário que as informações
+      constarem nos sistemas da Franet Telecom, indepente da data do envio dos
+      formulários. <br />
+      A promoção é válida para toda área de cobertura da Franet Telecom.
+      Regulamento e benefícios exclusivos para promoção indique & ganhe. Caso o
+      assinante Indicado busque por contratação em loja da Franet Telecom ou
+      qualquer outro meio fora da plataforma, não será concedido desconto ao
+      Cliente Indicador.<br />
+      O valor do benefício total será feito na proporção de 50% de uma
+      mensalidade integral do Cliente Indicador. O benefício não poderá ser
+      aplicado sobre mensalidade de cliente corporativo.
+    </p>
+  </div>
 </template>
 
 <script setup></script>
